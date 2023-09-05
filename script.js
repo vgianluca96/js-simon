669464e05ec955d97e971bd71c50fa5d1c9c1d45
vgianluca96 <gianluca.vallese@hotmail.com>
2023-09-05 15:58:00 +0200

    apportate modifiche nello style

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // Variabile con data finale
 let finalDate = new Date('09/06/2023 9:30:00');
 //console.log(finalDate);
-document.getElementById('description').innerHTML = ('Tempo mancante a ' + finalDate);
+document.getElementById('title').innerHTML = ('Tempo mancante a ' + finalDate);
 // Variabile associata al div
 let timeRemainingDiv = document.getElementById('timeRemaining');
 
@@ -14,21 +14,29 @@ setInterval(function () {
     
     // Tempo rimanente
     let timeRemaining = finalDate-now;
-    
-    // Converto il tempo rimanente in giorni, minuti, etc
-    let days = Math.floor(timeRemaining / (1000 * 60 * 60 *24));
-    let hours = Math.floor((timeRemaining % (1000 * 60 * 60 *24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((timeRemaining % (1000 * 60)) / (1000));
-    
-    // Creo stringa da stampare
-    let timeRemainingString = (days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's');
-    
-    //console.log(now);
-    //console.log(timeRemainingString);
 
-    // Stampa in pagina
-    timeRemainingDiv.innerHTML = timeRemainingString;
+    if (timeRemaining > 0) {
+        
+        // Converto il tempo rimanente in giorni, minuti, etc
+        let days = Math.floor(timeRemaining / (1000 * 60 * 60 *24));
+        let hours = Math.floor((timeRemaining % (1000 * 60 * 60 *24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((timeRemaining % (1000 * 60)) / (1000));
+        
+        // Creo stringa da stampare
+        let timeRemainingString = (days + 'd ' + hours + 'h ' + minutes + 'm ' + seconds + 's');
+        
+        //console.log(now);
+        //console.log(timeRemainingString);
+    
+        // Stampa in pagina
+        timeRemainingDiv.innerHTML = timeRemainingString;
 
+    } else {
+
+        timeRemainingDiv.innerHTML = 'Tempo scaduto';
+
+    }
+    
 }, 1000);
 
