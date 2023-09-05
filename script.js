addf3228ae19e9816c82484be032e95f3f9e8424
vgianluca96 <gianluca.vallese@hotmail.com>
2023-09-05 15:10:52 +0200

    terminata logica
    logica creata con stampa in console

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 
-console.log('ciao')
+
+// Variabile con data finale
+let finalDate = new Date('09/06/2023 9:30:00');
+//console.log(finalDate);
+
+setInterval(function () {
+
+    // Varabile con data attuale
+    let now = new Date();
+    
+    // Tempo rimanente
+    let timeRemaining = finalDate-now;
+    
+    // Converto il tempo rimanente in giorni, minuti, etc
+    let days = Math.floor(timeRemaining / (1000 * 60 * 60 *24));
+    let hours = Math.floor((timeRemaining % (1000 * 60 * 60 *24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((timeRemaining % (1000 * 60)) / (1000));
+    
+    // Creo stringa da stampare
+    let timeRemainingString = (days + 'd' + hours + 'h' + minutes + 'm' + seconds + 's');
+    
+    //console.log(now);
+    console.log(timeRemainingString);
+
+
+}, 1000);
+
